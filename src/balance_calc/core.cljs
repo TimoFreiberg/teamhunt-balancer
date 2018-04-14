10f1e9db46043f82041d044d2b60a28ad5f8933a
Timo Freiberg <timo.freiberg@gmail.com>
2018-04-14 23:00:30 +0200

    Start implementing player table

--- src/balance_calc/core.cljs
+++ src/balance_calc/core.cljs
@@ -1,25 +1,52 @@
 (ns balance-calc.core
-    (:require [reagent.core :as reagent :refer [atom]]))
+  (:require [reagent.core :as reagent :refer [atom]]))
 
 (enable-console-print!)
 
-(println "This text is printed from src/balance-calc/core.cljs. Go ahead and edit it and see reloading in action.")
-
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce ids (atom 0))
 
+(defonce app-state
+  (atom [(atom {:id 1
+                :name "test player"
+                :balance 25000})]))
 
-(defn hello-world []
+(defn player-row [player]
+  [:tr
+   [:td
+    [:input
+     {:type "text"
+      :alt "Player Name"
+      :value (:name @player)
+      :on-change (fn update-player-name
+                   [e]
+                   (swap! player #(assoc % :name (-> e .-target .-value))))}]]
+   [:td
+    [:input
+     {:type "text"
+      :value (:balance @player)}]]])
+
+(defn hunt-calc-page []
   [:div
-   [:h1 (:text @app-state)]
-   [:h3 "Edit this and watch it change!"]])
+   [:h1 "Team Hunt Balance Calculator"]
+   [:table
+    [:thead
+     [:tr
+      [:th "Player"]
+      [:th "Balance"]
+      [:th "Loot (optional)"]]]
+    [:tbody
+     (doall
+      (for [player @app-state]
+        ^{:key (:id @player)}
+        [player-row player]))]]])
 
-(reagent/render-component [hello-world]
+(reagent/render-component [hunt-calc-page]
                           (. js/document (getElementById "app")))
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+  )
