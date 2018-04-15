c6a74500649666462dee53fd82fdc85c6271c5da
Timo Freiberg <timo.freiberg@gmail.com>
2018-04-15 22:31:44 +0200

    Overall & average balance working

--- src/balance_calc/core.cljs
+++ src/balance_calc/core.cljs
@@ -1,5 +1,7 @@
 (ns balance-calc.core
-  (:require [reagent.core :as reagent :refer [atom]]))
+  (:require [reagent.core :as reagent :refer [atom]]
+            [goog.string :as gstring]
+            [goog.string.format]))
 
 (enable-console-print!)
 
@@ -7,25 +9,112 @@
 
 (defonce ids (atom 0))
 
+(defn get-new-id []
+  (let [id @ids
+        _ (swap! ids inc)]
+    id))
+
+(defn new-player []
+  (atom {:id (get-new-id)
+         :name ""
+         :balance nil
+         :loot nil}))
+
 (defonce app-state
-  (atom [(atom {:id 1
-                :name "test player"
-                :balance 25000})]))
+  (atom {:overall-balance 0
+         :average-balance 0}))
+;;FIXME instead of calculating balances, store them in the state, then display diff of actual and average balance per player
+
+(defn parse-num
+  "Parses a string into a number.
+  If the string ends with k, multiplies the number by 1000.
+  If the string ends with kk or m, multiplies the number by 1,000,000.
+  The suffix detection is case insensitive."
+  [s]
+  (if (nil? s)
+    0
+    (let [s (.toLowerCase s)
+          num (js/parseFloat s)]
+      (* num
+         (cond
+           (.endsWith s "kk") 1000000
+           (.endsWith s "m") 1000000
+           (.endsWith s "k") 1000
+           :else 1)))))
+
+(defn shorten-num
+  [n]
+  (cond
+    (>= n 1000000) (gstring/format
+                    "%.2fm"
+                    (/ n 1000000))
+    (>= n 1000) (gstring/format
+                 "%.2fk"
+                 (/ n 1000))
+    :else n))
+
+(defn player-attribute-text-field [player attribute]
+  [:input
+   {:type "text"
+    :value (attribute @player)
+    :on-change (fn update-player-attribute
+                 [e]
+                 (swap! player #(assoc % attribute (-> e .-target .-value))))}])
 
 (defn player-row [player]
   [:tr
    [:td
-    [:input
-     {:type "text"
-      :alt "Player Name"
-      :value (:name @player)
-      :on-change (fn update-player-name
-                   [e]
-                   (swap! player #(assoc % :name (-> e .-target .-value))))}]]
+    (player-attribute-text-field player :name)]
+   [:td
+    (player-attribute-text-field player :balance)]
+   [:td
+    ]
+   #_[:td
+      (player-attribute-text-field player :loot)]
    [:td
     [:input
-     {:type "text"
-      :value (:balance @player)}]]])
+     {:type "button"
+      :value "Remove Player"
+      :on-click (fn remove-player
+                  [_]
+                  (swap! app-state dissoc (:id @player)))}]]])
+
+(defn add-player-button
+  []
+  [:input
+   {:type "button"
+    :value "Add Player"
+    :on-click (fn add-player
+                [_]
+                (let [player (new-player)]
+                  (swap! app-state assoc (:id @player) player)))}])
+
+(defn get-balances
+  []
+  (->> (for [player (vals @app-state)]
+         (-> @player :balance parse-num))
+       (filter some?)))
+
+(defn overall-balance
+  []
+  [:div
+   "Overall balance: "
+   (->> (get-balances)
+        (reduce + 0)
+        shorten-num)])
+
+(defn average-balance
+  []
+  [:div
+   "Average balance: "
+   (let [player-count (count @app-state)]
+     (if (= 0 player-count)
+       0
+       (->
+        (->> (get-balances)
+             (reduce + 0))
+        (/ player-count)
+        shorten-num)))])
 
 (defn hunt-calc-page []
   [:div
@@ -38,9 +127,13 @@
       [:th "Loot (optional)"]]]
     [:tbody
      (doall
-      (for [player @app-state]
+      (for [player (vals @app-state)]
         ^{:key (:id @player)}
-        [player-row player]))]]])
+        [player-row player]))]]
+   [add-player-button]
+   [:h3 "Results:"]
+   [overall-balance]
+   [average-balance]])
 
 (reagent/render-component [hunt-calc-page]
                           (. js/document (getElementById "app")))
@@ -49,4 +142,5 @@
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
+  (println app-state)
   )
